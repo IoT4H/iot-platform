e1fbc373309a8f5f6fe802ba37ebfd5db38fec9f
Rickmann, Bastian <bastian.rickmann@accenture.com>
2025-05-27 21:41:04 +0200

    fix login portal

--- ui-ngx/src/app/modules/login/pages/login-portal/login-portal.component.ts
+++ ui-ngx/src/app/modules/login/pages/login-portal/login-portal.component.ts
@@ -21,7 +21,6 @@ import { AuthService } from '@core/auth/auth.service';
 import { AppState } from '@core/core.state';
 import { Store } from '@ngrx/store';
 import { PageComponent } from '@shared/components/page.component';
-import { OAuth2ClientInfo } from '@shared/models/oauth2.models';
 
 
 const IFRAME_LOGIN_READY = 'iframeReady';
@@ -36,23 +35,13 @@ const SENDING_LOGIN_TOKEN = 'sendingLoginToken';
 })
 export class LoginPortalComponent extends PageComponent implements OnInit {
 
-  oauth2Clients: Array<OAuth2ClientInfo> = null;
-
-  constructor(protected store: Store<AppState>,
-              private authService: AuthService,
-              public fb: UntypedFormBuilder,
-              private router: Router,
-              private activatedRoute: ActivatedRoute) {
-    super(store);
-    this.activatedRoute.queryParamMap.subscribe((params) => {
-      this.authService.redirectUrl = params.get('redirectUri');
-    });
-  }
-
   state = 'connecting';
 
   userInfo = '';
 
+  constructor(private authService: AuthService) {
+    super();
+  }
 
   ngOnInit() {
 
@@ -101,34 +90,5 @@ export class LoginPortalComponent extends PageComponent implements OnInit {
     }, 2000);
 
   }
-/*
-  login(): void {
-    if (this.loginFormGroup.valid) {
-      this.authService.login(this.loginFormGroup.value).subscribe(
-        () => {},
-        (error: HttpErrorResponse) => {
-          if (error && error.error && error.error.errorCode) {
-            if (error.error.errorCode === Constants.serverErrorCode.credentialsExpired) {
-              this.router.navigateByUrl(`login/resetExpiredPassword?resetToken=${error.error.resetToken}`);
-            } else if (error.error.errorCode === Constants.serverErrorCode.passwordViolation) {
-              this.passwordViolation = true;
-            }
-          }
-        }
-      );
-    } else {
-      Object.keys(this.loginFormGroup.controls).forEach(field => {
-        const control = this.loginFormGroup.get(field);
-        control.markAsTouched({onlySelf: true});
-      });
-    }
-  }*/
 
-  getOAuth2Uri(oauth2Client: OAuth2ClientInfo): string {
-    let result = '';
-    if (this.authService.redirectUrl) {
-      result += '?prevUri=' + this.authService.redirectUrl;
-    }
-    return oauth2Client.url + result;
-  }
 }
